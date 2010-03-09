edb561cf19a36db76600f1a5397c52fc9adf98c4
Hamish Barney <hbarney@atlassian.com>
2010-03-09 01:43:03 +0000

    Made methods on SkipInBrowserUtil public so they can be used outside the package.
    Fixed logic error in that class' skip method.

--- src/main/java/com/atlassian/selenium/SkipInBrowserUtil.java
+++ src/main/java/com/atlassian/selenium/SkipInBrowserUtil.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SkipInBrowserUtil {
-    static List<Class> filter(Browser browser, List<Class> list)
+    public static List<Class> filter(Browser browser, List<Class> list)
     {
         List<Class> filteredList = new ArrayList<Class>(list.size());
         for (Class<?> c : list)
         {
+            //Only added to the list if its not marked as to being to skip
             if(!skip(browser, c))
             {
                 filteredList.add(c);
@@ -17,19 +18,12 @@ public class SkipInBrowserUtil {
         return filteredList;
     }
 
-    static boolean skip(Browser browser, Class<?> c)
+    public static boolean skip(Browser browser, Class<?> c)
     {
         SkipInBrowser skipAnnotation = c.getAnnotation(SkipInBrowser.class);
-        if(skipAnnotation != null)
-        {
-                //Only added to the list if its not marked as to being to skip
-                if(!arrayContains(skipAnnotation.browsers(), browser))
-                {
-                    return true;
-                }
-        }
-        return false;
+        return (skipAnnotation != null) && arrayContains(skipAnnotation.browsers(), browser);
     }
+    
     static boolean arrayContains(Browser[] browsers, Browser searchBrowser)
     {
         for (Browser browser : browsers) {
