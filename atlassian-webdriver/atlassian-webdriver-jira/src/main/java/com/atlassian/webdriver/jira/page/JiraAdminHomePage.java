8eec1189aec8cc870fc9043b9c1d7026dbee56cd
Don Brown <dbrown@atlassian.com>
2010-10-08 02:58:27 +0000

    Working around lame javac bug

--- atlassian-webdriver/atlassian-webdriver-jira/src/main/java/com/atlassian/webdriver/jira/page/JiraAdminHomePage.java
+++ atlassian-webdriver/atlassian-webdriver-jira/src/main/java/com/atlassian/webdriver/jira/page/JiraAdminHomePage.java
@@ -1,16 +1,22 @@
 package com.atlassian.webdriver.jira.page;
 
+import com.atlassian.webdriver.jira.component.menu.AdminSideMenu;
 import com.atlassian.webdriver.page.AdminHomePage;
 import com.atlassian.webdriver.jira.JiraTestedProduct;
 
 /**
  *
  */
-public class JiraAdminHomePage extends JiraAdminAbstractPage<JiraAdminHomePage> implements AdminHomePage<JiraTestedProduct, JiraAdminHomePage>
+public class JiraAdminHomePage extends JiraAbstractPage<JiraAdminHomePage> implements AdminHomePage<JiraTestedProduct, JiraAdminHomePage>
 {
     protected JiraAdminHomePage(JiraTestedProduct testedProduct)
     {
         super(testedProduct, "/secure/admin");
+    }
+
+    public AdminSideMenu getAdminSideMenu()
+    {
+        return new AdminSideMenu(testedProduct);
     }
 
     
