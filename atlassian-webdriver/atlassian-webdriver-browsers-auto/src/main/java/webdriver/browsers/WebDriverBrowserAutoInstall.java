121bd996da5656ad01928aae63668e160f0952e1
Zhuang Xu <zxu@atlassian.com>
2010-11-23 06:59:56 +0000

    When closing the driver instance, suppressed a WebDriverException which could be thrown sometimes by WebDriver's RemoteWebDriver when it tries to close an already closed browser.

--- atlassian-webdriver/atlassian-webdriver-browsers-auto/src/main/java/webdriver/browsers/WebDriverBrowserAutoInstall.java
+++ atlassian-webdriver/atlassian-webdriver-browsers-auto/src/main/java/webdriver/browsers/WebDriverBrowserAutoInstall.java
@@ -3,8 +3,10 @@ package webdriver.browsers;
 import com.atlassian.webdriver.AtlassianWebDriver;
 import com.atlassian.webdriver.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 
 import java.io.File;
+import java.net.SocketException;
 
 /**
  * Client that supports automatically installing the appropriate browser for the environment
@@ -16,7 +18,7 @@ public enum WebDriverBrowserAutoInstall
 
     private AutoInstallConfiguration config;
     private AtlassianWebDriver driver;
-
+    
     WebDriverBrowserAutoInstall() {
         this.config = new AutoInstallConfiguration();
 
@@ -25,8 +27,21 @@ public enum WebDriverBrowserAutoInstall
             @Override
             public void run()
             {
-                driver.quit();
-            }
+                try
+                {
+                    driver.quit();
+                } catch (WebDriverException e)
+                {
+                    // WebDriver's RemoteWebDriver will try to close the browser
+                    // when it has already been closed, and as a result throws a
+                    // WebDriverException caused by a SocketException. We want
+                    // to ignore this exception.
+                    if (!(e.getCause() instanceof SocketException))
+                    {
+                        throw e;
+                    }
+                }
+           }
         });
 
         driver = WebDriverFactory.getDriver();
