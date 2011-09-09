7a204fc472e6304b5a39d3b4ebc6ee719ed5c50e
Jaie Wilson <jwilson@atlassian.com>
2011-09-09 08:03:01 +0000

    Updated to selenium 2.5 to get ready for firefox 6 support.

--- atlassian-webdriver/atlassian-webdriver-core/src/test/java/com/atlassian/webdriver/browsers/TestAutoBrowserInstaller.java
+++ atlassian-webdriver/atlassian-webdriver-core/src/test/java/com/atlassian/webdriver/browsers/TestAutoBrowserInstaller.java
@@ -11,7 +11,7 @@ import static junit.framework.Assert.assertTrue;
 public class TestAutoBrowserInstaller extends AbstractFileBasedServerTest
 {
     private static final String HTML_UNIT_USER_AGENT =
-        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3";
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8";
 
     UserAgentPage userAgentPage;
 
