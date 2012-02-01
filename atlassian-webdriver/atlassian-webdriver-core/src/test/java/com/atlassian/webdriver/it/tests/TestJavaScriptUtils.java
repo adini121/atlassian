ddbd5ff5d9db07ff53bf77b3c26bba4296cceb8b
Richard Atkins <richatkins@atlassian.com>
2012-02-01 06:33:59 +0000

    SELENIUM-175 JQuery and CSS hover aren't working in IE9  * Disabling these tests in IE for now  * Adding failing variants of the jquery test for Firefox, HtmlUnit and IE so if/when things improve, we know about it and can go and clean up the tests.

--- atlassian-webdriver/atlassian-webdriver-core/src/test/java/com/atlassian/webdriver/it/tests/TestJavaScriptUtils.java
+++ atlassian-webdriver/atlassian-webdriver-core/src/test/java/com/atlassian/webdriver/it/tests/TestJavaScriptUtils.java
@@ -5,15 +5,15 @@ import com.atlassian.webdriver.AtlassianWebDriver;
 import com.atlassian.webdriver.it.AbstractFileBasedServerTest;
 import com.atlassian.webdriver.it.pageobjects.page.JavaScriptUtilsPage;
 import com.atlassian.webdriver.testing.annotation.IgnoreBrowser;
+import com.atlassian.webdriver.testing.annotation.TestBrowser;
 import com.atlassian.webdriver.utils.MouseEvents;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Test for checking ByJquery functionality in Atlassian WebDriver.
@@ -36,7 +36,7 @@ public class TestJavaScriptUtils extends AbstractFileBasedServerTest
      * This test is testing that css psuedo class :hover will display an element
      */
     @Test
-    @IgnoreBrowser(value = {Browser.FIREFOX, Browser.HTMLUNIT}, reason = "CSS hovering on elements does not work.")
+    @IgnoreBrowser(value = {Browser.FIREFOX, Browser.HTMLUNIT, Browser.IE}, reason = "CSS hovering on elements does not work.")
     public void testCssHoverRespondsToMouseover()
     {
         WebElement hoveringDiv = driver.findElement(By.id("hovering-element"));
@@ -55,7 +55,7 @@ public class TestJavaScriptUtils extends AbstractFileBasedServerTest
     }
 
     @Test
-    @IgnoreBrowser(value = {Browser.FIREFOX, Browser.HTMLUNIT}, reason = "jQuery hovering on elements does not work.")
+    @IgnoreBrowser(value = {Browser.FIREFOX, Browser.HTMLUNIT, Browser.IE}, reason = "jQuery hovering on elements does not work.")
     public void testJQueryHoverRespondsToMouseover()
     {
         WebElement hoveringDiv = driver.findElement(By.id("hovering-jquery-element"));
@@ -73,6 +73,73 @@ public class TestJavaScriptUtils extends AbstractFileBasedServerTest
         assertTrue(driver.elementIsVisible(By.id("child-jquery-element-one")));
         assertTrue(driver.elementIsVisible(By.id("child-jquery-element-two")));
         assertTrue(driver.elementIsVisible(By.id("child-jquery-element-three")));
+    }
+
+    @Test
+    @TestBrowser("htmlunit")
+    public void testJQueryHoverBreaksForHtmlUnit()
+    {
+        WebElement hoveringDiv = driver.findElement(By.id("hovering-jquery-element"));
+        assertTrue(hoveringDiv.isDisplayed());
+
+        driver.waitUntilElementIsNotVisible(By.id("child-jquery-element-one"));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-one")));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-two")));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-three")));
+
+        // now hover over div to show the children
+        MouseEvents.hover(hoveringDiv, driver);
+        try
+        {
+            driver.waitUntilElementIsVisible(By.id("child-jquery-element-one"));
+            fail("htmlunit jquery hovers are working now");
+        }
+        catch (TimeoutException expected) {}
+    }
+
+    @Test
+    @TestBrowser("firefox")
+    public void testJQueryHoverBreaksForFirefox()
+    {
+        WebElement hoveringDiv = driver.findElement(By.id("hovering-jquery-element"));
+        assertTrue(hoveringDiv.isDisplayed());
+
+        driver.waitUntilElementIsNotVisible(By.id("child-jquery-element-one"));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-one")));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-two")));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-three")));
+
+        // now hover over div to show the children
+        MouseEvents.hover(hoveringDiv, driver);
+        try
+        {
+            driver.waitUntilElementIsVisible(By.id("child-jquery-element-one"));
+            fail("firefox jquery hovers are working now");
+        }
+        catch (TimeoutException expected) {}
+    }
+
+    // https://studio.atlassian.com/browse/SELENIUM-175
+    @Test
+    @TestBrowser("ie")
+    public void testJQueryHoverBreaksForIE()
+    {
+        WebElement hoveringDiv = driver.findElement(By.id("hovering-jquery-element"));
+        assertTrue(hoveringDiv.isDisplayed());
+
+        driver.waitUntilElementIsNotVisible(By.id("child-jquery-element-one"));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-one")));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-two")));
+        assertFalse(driver.elementIsVisible(By.id("child-jquery-element-three")));
+
+        // now hover over div to show the children
+        MouseEvents.hover(hoveringDiv, driver);
+        try
+        {
+            driver.waitUntilElementIsVisible(By.id("child-jquery-element-one"));
+            fail("ie jquery hovers are working now");
+        }
+        catch (TimeoutException expected) {}
     }
 
     @Test
