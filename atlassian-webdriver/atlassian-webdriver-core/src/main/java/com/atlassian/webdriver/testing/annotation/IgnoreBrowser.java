a544ccb2ce8e93b786ae6ee30c9c0204d6485216
Richard Atkins <richatkins@atlassian.com>
2012-02-01 02:20:06 +0000

    SELENIUM-153 Support inheriting IgnoreBrowser from a parent class, so certain test suites can run with exactly the browser they need.

--- atlassian-webdriver/atlassian-webdriver-core/src/main/java/com/atlassian/webdriver/testing/annotation/IgnoreBrowser.java
+++ atlassian-webdriver/atlassian-webdriver-core/src/main/java/com/atlassian/webdriver/testing/annotation/IgnoreBrowser.java
@@ -3,6 +3,7 @@ package com.atlassian.webdriver.testing.annotation;
 import com.atlassian.pageobjects.Browser;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -17,6 +18,7 @@ import java.lang.annotation.Target;
  */
 @Retention (RetentionPolicy.RUNTIME)
 @Target ({ElementType.METHOD, ElementType.TYPE})
+@Inherited
 public @interface IgnoreBrowser
 {
     Browser[] value() default {Browser.ALL};
