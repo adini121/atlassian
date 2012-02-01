4e323318b696b92d9557ed5b351ba06ac7ee18dd
Jed Wesley-Smith <jwesleysmith@atlassian.com>
2012-02-01 04:10:19 +0000

    SELENIUM-174 Enable programmatic browser selection. Also added some javadocs.

--- atlassian-webdriver/atlassian-webdriver-core/src/main/java/com/atlassian/webdriver/browsers/AutoInstallConfiguration.java
+++ atlassian-webdriver/atlassian-webdriver-core/src/main/java/com/atlassian/webdriver/browsers/AutoInstallConfiguration.java
@@ -2,41 +2,47 @@ package com.atlassian.webdriver.browsers;
 
 import com.atlassian.browsers.BrowserAutoInstaller;
 import com.atlassian.browsers.BrowserConfig;
+import com.atlassian.browsers.BrowserConfiguration;
 import com.atlassian.browsers.InstallConfigurator;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-
 /**
- * 
- *
  * @since 2.0
  */
 public class AutoInstallConfiguration
 {
+    /**
+     * Setup a browser using system props to determine which, using maven's taget dir as a temp dir.
+     */
     public static BrowserConfig setupBrowser()
     {
+        return setupBrowser(new WebDriverBrowserConfiguration());
+    }
+
+    /**
+     * Setup a browser specifying the browser and temp dir.
+     */
+    public static BrowserConfig setupBrowser(final BrowserConfiguration browserConfiguration)
+    {
         final AtomicReference<BrowserConfig> ref = new AtomicReference<BrowserConfig>();
-        BrowserAutoInstaller browserAutoInstaller = new BrowserAutoInstaller(new WebDriverBrowserConfiguration(),
-            new InstallConfigurator() {
+        final BrowserAutoInstaller browserAutoInstaller = new BrowserAutoInstaller(browserConfiguration, new InstallConfigurator()
+        {
 
-                @Override
-                public void setupFirefoxBrowser(final BrowserConfig browserConfig)
-                {
-                    ref.set(browserConfig);
-                    System.setProperty("webdriver.firefox.bin", browserConfig.getBinaryPath());
-                    //System.setProperty("webdriver.firefox.profile", browserConfig.getProfilePath());
-                }
-
-                @Override
-                public void setupChromeBrowser(final BrowserConfig browserConfig)
-                {
-                    ref.set(browserConfig);
-                    System.setProperty("webdriver.chrome.bin", browserConfig.getBinaryPath());
-                }
+            @Override
+            public void setupFirefoxBrowser(final BrowserConfig browserConfig)
+            {
+                ref.set(browserConfig);
+                System.setProperty("webdriver.firefox.bin", browserConfig.getBinaryPath());
             }
-        );
 
+            @Override
+            public void setupChromeBrowser(final BrowserConfig browserConfig)
+            {
+                ref.set(browserConfig);
+                System.setProperty("webdriver.chrome.bin", browserConfig.getBinaryPath());
+            }
+        });
         browserAutoInstaller.setupBrowser();
         return ref.get();
     }
--- atlassian-webdriver/atlassian-webdriver-core/src/main/java/com/atlassian/webdriver/browsers/WebDriverBrowserConfiguration.java
+++ atlassian-webdriver/atlassian-webdriver-core/src/main/java/com/atlassian/webdriver/browsers/WebDriverBrowserConfiguration.java
@@ -5,12 +5,17 @@ import com.atlassian.browsers.BrowserType;
 
 import java.io.File;
 
+/**
+ * A default BrowserConfiguration that reads the specific browser from System properties 
+ * and uses the maven 'target' directory as a tmp dir to install the driver in if auto-installing it.
+ */
 public class WebDriverBrowserConfiguration implements BrowserConfiguration
 {
     private final File targetDir = new File("target");
     private final File webdriverDir = new File(targetDir, "webdriverTmp");
 
-    public WebDriverBrowserConfiguration() {}
+    public WebDriverBrowserConfiguration()
+    {}
 
     public File getTmpDir()
     {
