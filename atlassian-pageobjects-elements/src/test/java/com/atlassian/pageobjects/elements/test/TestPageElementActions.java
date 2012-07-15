c555efd48ccd1d7ca90dd44e4c3608405bbe0bfd
Dariusz Kordonski <dkordonski@atlassian.com>
2012-07-15 05:48:49 +0000

    unignroing test that should now work in chrome

--- atlassian-pageobjects-elements/src/test/java/com/atlassian/pageobjects/elements/test/TestPageElementActions.java
+++ atlassian-pageobjects-elements/src/test/java/com/atlassian/pageobjects/elements/test/TestPageElementActions.java
@@ -29,7 +29,6 @@ public class TestPageElementActions extends AbstractFileBasedServerTest
     }
 
     @Test
-    @IgnoreBrowser(value = Browser.CHROME, reason = "http://code.google.com/p/selenium/issues/detail?id=3851")
     public void shouldGetActionsViaContext()
     {
         final PageElementActions actions = product.getPageBinder().bind(PageObjectWithActions.class).actions;
@@ -38,7 +37,6 @@ public class TestPageElementActions extends AbstractFileBasedServerTest
     }
 
     @Test
-    @IgnoreBrowser(value = Browser.CHROME, reason = "http://code.google.com/p/selenium/issues/detail?id=3851")
     public void shouldGetActionsViaPageBinder()
     {
         final PageElementActions actions = product.getPageBinder().bind(PageElementActions.class);
