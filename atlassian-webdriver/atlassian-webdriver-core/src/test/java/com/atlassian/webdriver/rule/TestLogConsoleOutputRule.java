c0604845ce315a2f7a0e0924870756ddebc022dd
Chris Darroch <cdarroch@atlassian.com>
2014-11-18 17:41:39 +1100

    SELENIUM-237 - Add tests for checking what the rule does in response to detecting errors.

--- atlassian-webdriver/atlassian-webdriver-core/src/test/java/com/atlassian/webdriver/rule/TestLogConsoleOutputRule.java
+++ atlassian-webdriver/atlassian-webdriver-core/src/test/java/com/atlassian/webdriver/rule/TestLogConsoleOutputRule.java
@@ -1,14 +1,22 @@
 package com.atlassian.webdriver.rule;
 
 import com.atlassian.webdriver.testing.rule.LogConsoleOutputRule;
+import com.google.common.collect.Lists;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -23,6 +31,16 @@ public class TestLogConsoleOutputRule
 {
     @Mock private Logger mockLogger;
 
+    private List<String> errorsFound;
+    private boolean failOnErrorsFound;
+
+    @Before
+    public void setUp()
+    {
+        errorsFound = Lists.newArrayList();
+        failOnErrorsFound = false;
+    }
+
     @Test
     public void testOutputsSpecialMessageWhenCannotRetrieveConsoleErrorsOnTestFailed() throws Exception
     {
@@ -34,8 +52,53 @@ public class TestLogConsoleOutputRule
         verifyNoMoreInteractions(mockLogger);
     }
 
+    @Test
+    public void testLogsWhenNoErrorsFound()
+    {
+        final FirefoxDriver driver = mock(FirefoxDriver.class);
+        final LogConsoleOutputRule rule = createRule(driver);
+
+        rule.finished(Description.createTestDescription(TestLogConsoleOutputRule.class, "testMethod"));
+        verify(mockLogger).info("----- Test '{}' finished with {} JS error(s). ", "testMethod", 0);
+        verifyNoMoreInteractions(mockLogger);
+    }
+
+    @Test
+    public void testCanBeOverriddenToFailTestWhenErrorsFound()
+    {
+        final FirefoxDriver driver = mock(FirefoxDriver.class);
+        final LogConsoleOutputRule rule = createRule(driver);
+
+        errorsFound.add("TypeError: $ is not a function");
+        failOnErrorsFound = true;
+
+        try
+        {
+            rule.finished(Description.createTestDescription(TestLogConsoleOutputRule.class, "testMethod"));
+            assertThat("Expected an exception to be thrown, but wasn't", true, equalTo(false));
+        }
+        catch (RuntimeException e)
+        {
+            assertThat(e.getMessage(), containsString("Test failed due to javascript errors being detected"));
+        }
+        verify(mockLogger).info("----- Test '{}' finished with {} JS error(s). ", "testMethod", 1);
+    }
+
     private LogConsoleOutputRule createRule(final WebDriver driver)
     {
-        return new LogConsoleOutputRule(driver, mockLogger);
+        return new LogConsoleOutputRule(driver, mockLogger)
+        {
+            @Override
+            protected boolean shouldFailOnJavaScriptErrors()
+            {
+                return failOnErrorsFound;
+            }
+
+            @Override
+            protected List<String> errors()
+            {
+                return errorsFound;
+            }
+        };
     }
 }
