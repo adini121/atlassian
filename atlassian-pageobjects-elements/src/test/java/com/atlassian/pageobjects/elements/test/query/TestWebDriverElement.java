59990386b0f7163b932cd47511891c3729c807f9
Dariusz Kordonski <dkordonski@atlassian.com>
2015-03-30 16:59:29 +1100

    SELENIUM-239 fixing broken unit test

--- atlassian-pageobjects-elements/src/test/java/com/atlassian/pageobjects/elements/test/query/TestWebDriverElement.java
+++ atlassian-pageobjects-elements/src/test/java/com/atlassian/pageobjects/elements/test/query/TestWebDriverElement.java
@@ -4,10 +4,8 @@ import com.atlassian.pageobjects.PageBinder;
 import com.atlassian.pageobjects.ProductInstance;
 import com.atlassian.pageobjects.Tester;
 import com.atlassian.pageobjects.binder.InjectPageBinder;
-import com.atlassian.pageobjects.elements.PageElement;
 import com.atlassian.pageobjects.elements.WebDriverElement;
 import com.atlassian.pageobjects.elements.timeout.Timeouts;
-import com.atlassian.webdriver.AtlassianWebDriver;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import org.junit.Before;
@@ -15,11 +13,12 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import static org.mockito.Mockito.when;
 
 /**
- * Test case for {@link com.atlassian.pageobjects.elements.WebDriverElement}.
+ * Test case for {@link WebDriverElement}.
  *
  * @since 2.0.0
  */
@@ -32,7 +31,7 @@ public class TestWebDriverElement
     private Tester mockTester;
 
     @Mock
-    private AtlassianWebDriver mockDriver;
+    private WebDriver mockDriver;
 
     @Mock
     private Timeouts mockTimeouts;
@@ -46,7 +45,7 @@ public class TestWebDriverElement
         when(mockProductInstance.getBaseUrl()).thenReturn("http://test.atlassian.com:8080/test");
         pageBinder = new InjectPageBinder(mockProductInstance, mockTester, new Module() {
             public void configure(Binder binder) {
-                binder.bind(AtlassianWebDriver.class).toInstance(mockDriver);
+                binder.bind(WebDriver.class).toInstance(mockDriver);
                 binder.bind(Timeouts.class).toInstance(mockTimeouts);
             }
         });
@@ -57,7 +56,6 @@ public class TestWebDriverElement
     {
         final WebDriverElement tested = pageBinder.bind(WebDriverElement.class, By.id("test-id"));
 
-        PageElement element = tested.find(By.className("inside"));
-
+        tested.find(By.className("inside"));
     }
 }
