6b574821f2aa2eb8c086bd9239862ab616e592d6
Dariusz Kordonski <dkordonski@atlassian.com>
2015-04-02 15:44:07 +1100

    SELENIUM-239 added package-info for the search package

---
+++ atlassian-pageobjects-elements/src/main/java/com/atlassian/pageobjects/elements/search/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains API for advanced DOM search functionality. See
+ * {@link com.atlassian.pageobjects.elements.search.PageElementSearch} for the detailed description of how to use the
+ * API.
+ *
+ * @see com.atlassian.pageobjects.elements.search.PageElementSearch
+ */
+package com.atlassian.pageobjects.elements.search;
